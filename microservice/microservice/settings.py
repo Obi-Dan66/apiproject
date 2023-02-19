f108692d9a2595f263a0ea2c81107729da6ea7a2
Daniel Prochazka <danielprochazka@gmail.com>
2023-02-19 08:06:45 +0100

    add confsecrets file

--- microservice/microservice/settings.py
+++ microservice/microservice/settings.py
@@ -11,14 +11,14 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-from confsecretrs import SECRET_KEY
+from confsecrets import SECRET_KEY
 
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+SECRET_KEY = SECRET_KEY
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
