9f29ad4190323496fafa464e65a611d599a0e4ec
Daniel Prochazka <danielprochazka@gmail.com>
2023-02-22 23:19:29 +0100

    new

--- microservice/microservice/settings.py
+++ microservice/microservice/settings.py
@@ -11,14 +11,16 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-from confsecrets import SECRET_KEY
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = SECRET_KEY
+SECRET_KEY = os.getenv("SECRET_KEY")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
