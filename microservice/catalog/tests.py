02455a078afba1934c1f13078bc0401ae52fc280
Daniel Prochazka <danielprochazka@gmail.com>
2023-02-23 04:22:57 +0100

    blank api

--- microservice/catalog/tests.py
+++ microservice/catalog/tests.py
@@ -1,59 +1,66 @@
-# from django.test import TestCase
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APITestCase, APIClient
+from rest_framework.test import APIClient
+from .models import Product
+from .serializers import ProductSerializer
 
-from .models import Todo
-
-
-class TodoModelTest(TestCase):
-    def setUp(self):
-        self.todo = Todo.objects.create(title="Test Todo", completed=False)
-
-    def test_todo_model(self):
-        self.assertTrue(isinstance(self.todo, Todo))
-        self.assertEqual(str(self.todo), self.todo.title)
-
-
-class TodoViewTest(APITestCase):
+class ProductTestCase(TestCase):
     def setUp(self):
         self.client = APIClient()
-        self.todo = Todo.objects.create(title="Test Todo", completed=False)
+        self.product_data = {
+            "name": "Test Product",
+            "description": "This is a test product",
+            "price": 19.99
+        }
+        self.response = self.client.post(
+            reverse("product-list"), self.product_data, format="json"
+        )
 
-    def test_get_all_todos(self):
-        response = self.client.get(reverse("todo-list"))
+    def test_create_product(self):
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Product.objects.count(), 1)
+        self.assertEqual(
+            Product.objects.get().name, self.product_data["name"]
+        )
+        self.assertEqual(
+            Product.objects.get().description, self.product_data["description"]
+        )
+        self.assertEqual(
+            Product.objects.get().price, self.product_data["price"]
+        )
+
+    def test_get_product_list(self):
+        response = self.client.get(reverse("product-list"))
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        self.assertEqual(response.data, serializer.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_create_todo_with_valid_data(self):
-        data = {"title": "New Todo", "completed": False}
-        response = self.client.post(reverse("todo-list"), data)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Todo.objects.count(), 2)
-
-    def test_create_todo_with_invalid_data(self):
-        data = {"title": "", "completed": False}
-        response = self.client.post(reverse("todo-list"), data)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
-    def test_get_single_todo(self):
-        response = self.client.get(reverse("todo-detail", args=[self.todo.id]))
+    def test_get_product_detail(self):
+        product = Product.objects.get()
+        response = self.client.get(reverse("product-detail", args=[product.id]))
+        serializer = ProductSerializer(product)
+        self.assertEqual(response.data, serializer.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_update_todo_with_valid_data(self):
-        data = {"title": "Updated Todo", "completed": True}
-        response = self.client.put(reverse("todo-detail", args=[self.todo.id]), data)
+    def test_update_product(self):
+        product = Product.objects.get()
+        new_product_data = {
+            "name": "Updated Product",
+            "description": "This is an updated product",
+            "price": 29.99
+        }
+        response = self.client.put(
+            reverse("product-detail", args=[product.id]), new_product_data, format="json"
+        )
+        updated_product = Product.objects.get()
+        serializer = ProductSerializer(updated_product)
+        self.assertEqual(response.data, serializer.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.todo.refresh_from_db()
-        self.assertEqual(self.todo.title, "Updated Todo")
-        self.assertEqual(self.todo.completed, True)
 
-    def test_update_todo_with_invalid_data(self):
-        data = {"title": "", "completed": True}
-        response = self.client.put(reverse("todo-detail", args=[self.todo.id]), data)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
-    def test_delete_todo(self):
-        response = self.client.delete(reverse("todo-detail", args=[self.todo.id]))
+    def test_delete_product(self):
+        product = Product.objects.get()
+        response = self.client.delete(reverse("product-detail", args=[product.id]))
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        self.assertEqual(Todo.objects.count(), 0)
+        self.assertEqual(Product.objects.count(), 0)
